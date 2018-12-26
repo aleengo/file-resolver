13639e99e54381a37dde664062d98744cbab262d
gnemoka <gnemo.ka@gmail.com>
2018-12-26 10:38:51 +0100

    project initialization

---
+++ test/file-resolver.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const fileResolver = require('../lib');
+
+describe('file-resolver', function() {
+    it('Should throw an TypeError when dirname is not a string', function() {
+
+    });
+});
